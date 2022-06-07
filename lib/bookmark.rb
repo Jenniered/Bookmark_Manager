0e8cbf4a812f289f03cc84b3d8b54b3c9b0679a8
Jennie Evans <jennie1evans@hotmail.com>
2022-06-07 18:18:40 +0100

    Interacting with PostgresSQL from Ruby

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,16 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-     ]
+    connection = PG.connect(dbname: "bookmark_manager")
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url']}
   end
+
+  #   [
+  #     "http://www.makersacademy.com",
+  #     "http://www.destroyallsoftware.com",
+  #     "http://www.google.com"
+  #    ]
+  # end
 end
