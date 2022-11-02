7adc0bbc510f09f58171587354f807957b8b79e6
jennieevans <jennie1evans@hotmail.com>
2022-11-02 12:50:48 +0000

    tag_spec tests passing

--- spec/bookmark_tag_spec.rb
+++ spec/bookmark_tag_spec.rb
@@ -5,11 +5,7 @@ describe BookmarkTag do
   describe '.create' do
     it 'creates a link between a bookmark and a tag' do
       bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
-      # DatabaseConnection.query("INSERT INTO tags (id, content) VALUES (1, 'test tag');")
-      # DatabaseConnection.query("INSERT INTO bookmark_tags (bookmark_id, tag_id) VALUES ($1, 1)", [bookmark.id])
 
-      # tag = bookmark.tags.first
-      # expect(tag['content']).to eq 'test tag'
       tag = Tag.create(content: 'test tag')
       bookmark_tag = BookmarkTag.create(bookmark_id: bookmark.id, tag_id: tag.id)
 
